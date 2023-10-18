7de3336c1c6c8b7d6ac36fa59adc168e9708ace9
Duskire <duskire@gmail.com>
2023-10-19 11:23:26 +1300

    Updated navbar and buttons

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -10,17 +10,17 @@ function NavLink({to, children}) {
 
 export default function Navbar(user, current_page) {
     return (
-        <nav className="fixed rounded-b-lg w-400px z-2 top-0 left-0 h-16 bg-primary-800 border-b-2 border-r-2 border-accent-500">
-            <div className="max-w-screen-xl flex items-center justify-center mx-auto p-2">
+        <nav className="absolute justify-center rounded-lg z-2 object-top top-2 inset-x-80 h-16 bg-primary-700 border-b-2 border-r-2 border-accent-500">
+            <div className="flex items-center justify-center mx-auto p-2">
                 <ul className="flex flex-row text-2xl">
                     <li>
-                        <a href="/" className="inline-block py-2 px-5 text-center rounded border-r-2 text-text-200" id="home-button" value="Home">Home</a>
+                        <a href="/" className="inline-block py-2 px-5 text-center rounded text-text-200 hover:text-text-600" id="home-button" value="Home">Home</a>
                     </li>
                     <li>
-                        <a href="/projects" className="inline-block py-2 px-5 text-center rounded border-r-2 text-text-200" id="projects-button" value="Projects">Projects</a>
+                        <a href="/#projects" className="inline-block py-2 px-5 text-center rounded border-r-2 text-text-200 hover:text-text-600" id="projects-button" value="Projects">Projects</a>
                     </li>
                     <li>
-                        <a href="/about" className="inline-block py-2 px-5 text-center rounded text-text-200" id="about-button" value="About">About</a>
+                        <a href="/about" className="inline-block py-2 px-5 text-center rounded text-text-200 hover:text-text-600" id="about-button" value="About">About</a>
                     </li>
                 </ul>
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -16,49 +16,54 @@ export default function Home() {
         <div className="relative mt-40 mx-auto p-5 z-2 object-center overflow-hidden pointer-events-none">
           <Image className="relative transform hover:scale-120 priority" src="/banner.svg" alt="Duskire Banner" width={480} height={360} id="banner"/>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row z-2">
           <a href="https://www.twitch.tv/duskirelive" className="">
             <Image className="border-box h-20 w-20 p-3" src="/twitchicon.svg" alt="twitch" width={48} height={48} id="social1"/>
           </a>
-          <a href="https://www.youtube.com/channel/UC6UMmFrvEXOQ9JptEpjSHpA" className="">
+          <a href="https://www.youtube.com/@duskirelive" className="">
             <Image className="border-box h-20 w-20 p-3" src="/youtubeicon.svg" alt="youtube" width={48} height={48} id="social2"/>
           </a>
-          <a href="https://x.com/duskirelive" className="">
+          <a href="https://www.x.com/duskirelive" className="">
             <Image className="border-box h-20 w-20 p-3" src="/twitxicon.svg" alt="twitterx" width={48} height={48} id="social3"/>
           </a>
           <a href="https://www.reddit.com/user/DuskireLive" className="">
             <Image className="border-box h-20 w-20 p-3" src="/redditicon.svg" alt="reddit" width={48} height={48} id="social4"/>
           </a>
-          <a href="https://github.com/Duskire" className="">
+          <a href="https://www.github.com/Duskire" className="">
             <Image className="border-box h-20 w-20 p-3" src="/githubicon.svg" alt="github" width={48} height={48} id="social5"/>
           </a>
         </div>
-        <div className="relative mx-auto p-10 text-xl">
-          <a href="#projects" className="text-2xl"><u>Projects</u></a>
-          <h2 className="relative p-3 text-text-300" id="projects">Here are some of my current and upcoming projects</h2>
-          <div className="">
-            <button className="inline-block p-2 rounded-xl text-text-500 bg-secondary-800 hover:text-text-200">
-              This Site!
-              <Image className="" src="/publicicon.svg" alt="Public Icon" width={30} height={30}/>
-            </button>
-          </div>
-          <div className="">
-            <button className="inline-block p-2 rounded-xl text-text-500 bg-secondary-800 hover:text-text-200">
-              Coming Soon!
-              <Image className="" src="/scheduleicon.svg" alt="Schedule Icon" width={30} height={30}/>
-            </button>
-          </div>
-          <div className="">
-            <button className="inline-block p-2 rounded-xl text-text-500 bg-secondary-800 hover:text-text-200">
-              Coming Soon!
-              <Image className="" src="/scheduleicon.svg" alt="Schedule Icon" width={30} height={30}/>
-            </button>
+        <div className="relative mx-auto p-20 z-2 text-xl">
+          <p className="">I'm a programming student who has a lot to share with the world!</p>
+          <hr />
+          <p className="text-2xl p-4 text-text-300" id="projects"><b>Projects</b></p>
+          <h2 className="relative p-3 text-text-300">Here are some of my current and upcoming projects</h2>
+          <div className="flex flex-row items-center justify-center">
+            <div className="p-3">
+              <button className="inline-block p-3 rounded text-text-200 bg-primary-500 hover:text-text-400">
+                This Site!
+                <Image className="mx-auto" src="/javascript.jpg" alt="Javascript Image" width={150} height={150}/>
+              </button>
+            </div>
+            <div className="p-3">
+              <button className="inline-block p-3 rounded text-text-200 bg-primary-500 hover:text-text-400">
+                Coming Soon!
+                <Image className="mx-auto" src="/comingsoon.jpg" alt="Coming Soon Image" width={150} height={150}/>
+              </button>
+            </div>
+            <div className="p-3 hover:border-5 hover:border-accent-300">
+              <button className="inline-block p-3 rounded text-text-200 bg-primary-500 hover:text-text-400">
+                Coming Soon!
+                <Image className="mx-auto" src="/comingsoon.jpg" alt="Coming Soon Image" width={150} height={150}/>
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </main>
   )
 }
+
 
   /*return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
